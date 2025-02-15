d0df0b6dd25df02513344061d89becfc3026ea8e
Dao Hai Nam <daohainam@gmail.com>
2025-02-15 20:55:34 +0100

    Add Variant class to ProductCatalog.Api.Models

---
+++ ProductCatalog.Api/Models/Variant.cs
@@ -0,0 +1,12 @@
+﻿namespace ProductCatalog.Api.Models
+{
+    public class Variant
+    {
+        public Guid Id { get; set; }
+        public Guid ProductId { get; set; }
+        public string Sku { get; set; } = default!;
+        public string Name { get; set; } = default!;
+        public double Price { get; set; }
+        public int AvailableStock { get; set; }
+    }
+}

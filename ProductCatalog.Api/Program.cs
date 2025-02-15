2adbd2cc06caaab68f2130903a5ead6ae1c34e7c
Dao Hai Nam <daohainam@gmail.com>
2025-02-15 13:19:51 +0100

    Refactor service setup and add OpenAPI configuration

--- ProductCatalog.Api/Program.cs
+++ ProductCatalog.Api/Program.cs
@@ -4,10 +4,6 @@ using ProductCatalog.ServiceDefaults;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.AddServiceDefaults();
-
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
 builder.AddApplicationServices();
 
 var app = builder.Build();
